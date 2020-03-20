fb84aad667feff2c096732f3ee80f3b1871f9682
William Raymann <raymann@ksu.edu>
2020-03-20 15:24:23 -0500

    Added documentation to static class ExtensionMethods and changed the UML to accurately signify static classes, multiplicity in relationships, and types of relationships.

--- PointOfSale/ExtensionMethods.cs
+++ PointOfSale/ExtensionMethods.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Author: Nathan Bean
+ * Written by: William Raymann
+ * Class: ExtensionMethods
+ * Purpose: To make additional functionality to classes withing the PointOfSale 
+ *          namespace available as needed.
+ */
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -8,6 +15,13 @@ namespace PointOfSale
 {
     public static class ExtensionMethods
     {
+        /// <summary>
+        /// Finds the first parent of the calling element that is
+        /// the type specified by the calling element.
+        /// </summary>
+        /// <typeparam name="T">The type of parent we are looking for.</typeparam>
+        /// <param name="element">The calling element.</param>
+        /// <returns>The first parent of type T.</returns>
         public static T FindAncestor<T>(this DependencyObject element) where T : DependencyObject
         {
             var parent = VisualTreeHelper.GetParent(element);
